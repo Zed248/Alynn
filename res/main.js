90fd8514f2073cf5dfc8e9c6be473072dd92734b
Rakkha <linnayfb@gmail.com>
2023-06-14 16:32:35 +0630

    99

---
+++ res/main.js
@@ -0,0 +1,16 @@
+var body = document.querySelector('body');
+
+let navContents = "Home"
+
+function createNav(items) {
+    let nav = document.createElement('nav');
+    nav.textContent = items;
+    nav.className = "nav-bar"
+    body.appendChild(nav);
+}
+
+
+function 
+
+
+createNav()
